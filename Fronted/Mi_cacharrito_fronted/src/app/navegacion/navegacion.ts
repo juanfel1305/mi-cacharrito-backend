dc38dbc08aec131fe50c3f93a087959b78bcbf2b
sergio281 <calderonsergio645@gmail.com>
2026-09-23 10:26:47 -0500

    Delete Fronted/Mi_cacharrito_fronted directory
    Eliminada carpeta frontend va ser utilizada en un repositorio nuevo

--- Fronted/Mi_cacharrito_fronted/src/app/navegacion/navegacion.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-navegacion',
-  styleUrl: './navegacion.css',
-  templateUrl: './navegacion.html',
-})
-export class Navegacion {}

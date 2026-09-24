24652d219d30b70fb8a3279355831e0ab4ffe460
Sergio Fernando Calderon <calderonsergio645@gmail.com>
2026-09-24 00:23:56 -0500

    Actualizacion de controlador, modelos y repositorio de usuario

--- Backend/src/main/java/Mi_cacharrito/controlador/ControladorUsuario.java
+++ Backend/src/main/java/Mi_cacharrito/controlador/ControladorUsuario.java
@@ -2,30 +2,35 @@ package Mi_cacharrito.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import Mi_cacharrito.modelo.Usuario;
+import Mi_cacharrito.repositorio.RepositorioUsuario;
 
-
-
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ControladorUsuario {
-	
+
 	@Autowired
-	private ControladorUsuario repoUsuarioControlador;
+	private RepositorioUsuario repoUsuario;
 
-	
-	@PostMapping("/guardarUsuario/")
-	public ResponseEntity<ControladorUsuario> guardar(@RequestBody ControladorUsuario u){
-	    
-	    
-	    return ResponseEntity.ok(u);
+	@PostMapping("/RegistrioUsuarios")
+	public ResponseEntity<?> guardar(@RequestBody Usuario u) {
+
+		System.out.println("CONTROLADOR NUEVO");
+
+		if (repoUsuario.existsByDocumento(u.getDocumento())) {
+			return ResponseEntity.status(409).body("Ya existe una cuenta con ese número de documento");
+		}
+
+		if (repoUsuario.existsByCorreo(u.getCorreo())) {
+			return ResponseEntity.status(409).body("Ya existe una cuenta con ese correo electrónico");
+		}
+
+		Usuario guardado = repoUsuario.save(u);
+		return ResponseEntity.ok(guardado);
 	}
-
-
 }
-
-
-
-
-		
-

3222dea1c7f139e896908d792d60eb71c554f8bd
Alekai1631 <laura.341911527@ucaldas.edu.co>
2026-09-25 20:47:33 -0500

    Backen de iniciar sesion para admin

--- src/main/java/Mi_cacharrito/repositorio/RepositorioAdministrador.java
+++ src/main/java/Mi_cacharrito/repositorio/RepositorioAdministrador.java
@@ -1,5 +1,10 @@
 package Mi_cacharrito.repositorio;
 
-public interface RepositorioAdministrador {
+import org.springframework.data.jpa.repository.JpaRepository;
+import Mi_cacharrito.modelo.Administrador;
 
+public interface RepositorioAdministrador extends JpaRepository<Administrador, Integer> {
+
+	    public Administrador findByUsername(String username);
+	
 }
